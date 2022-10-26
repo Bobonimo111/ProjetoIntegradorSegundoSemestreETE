7d10ae89130512aa887357e05399e9a9359250df
Bobonimo111 <carlosmaycon443@gmail.com>
2022-10-26 15:55:47 -0300

    Inital commit

---
+++ src/insertDb/EstoqueInsert.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package insertDb;
+
+/**
+ *
+ * @author Aluno
+ */
+public class EstoqueInsert {
+    
+    private int item;
+    private int quantidade;
+    private String validade;
+    private double valor_compra;
+    private double venda_valor;
+
+    /**
+     * @return the item
+     */
+    public int getItem() {
+        return item;
+    }
+
+    /**
+     * @param item the item to set
+     */
+    public void setItem(int item) {
+        this.item = item;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the validade
+     */
+    public String getValidade() {
+        return validade;
+    }
+
+    /**
+     * @param validade the validade to set
+     */
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    /**
+     * @return the valor_compra
+     */
+    public double getValor_compra() {
+        return valor_compra;
+    }
+
+    /**
+     * @param valor_compra the valor_compra to set
+     */
+    public void setValor_compra(double valor_compra) {
+        this.valor_compra = valor_compra;
+    }
+
+    /**
+     * @return the venda_valor
+     */
+    public double getVenda_valor() {
+        return venda_valor;
+    }
+
+    /**
+     * @param venda_valor the venda_valor to set
+     */
+    public void setVenda_valor(double venda_valor) {
+        this.venda_valor = venda_valor;
+    }
+    
+}
---
+++ src/main/Main.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import Cadastros.*;
+import javax.swing.JFrame;
+
+
+
+public class Main {
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+        Tela_inicial tela_inicial = new Tela_inicial();
+                
+        tela_inicial.setVisible(true);
+        
+        
+        
+        
+        
+    }
+    /*
+    public static void tela(boolean ativado, String tela){
+    
+    Cliente cliente = new Cliente();
+    Funcionario cadFun = new Funcionario();
+    
+    Item item = new Item();
+    Pedido pedido = new Pedido();
+    Estoque estoque = new Estoque();
+    
+    
+        if(tela.toLowerCase() == "cliente"){
+             cliente.setVisible(ativado);
+        }
+        else if(tela.toLowerCase() == "funcioanario"){
+            cadFun.setVisible(ativado);
+        }
+        else if(tela.toLowerCase() == )
+              
+    }
+    */
+    
+    
+}
